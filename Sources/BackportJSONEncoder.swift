8b54533ddf6d87f0d9f973803a0c103d4faa6fc1
Sergej Jaskiewicz <seergy_pro@me.com>
2018-03-21 17:04:10 +0300

    Attempt fixing build failure for Xcode 8

--- Sources/BackportJSONEncoder.swift
+++ Sources/BackportJSONEncoder.swift
@@ -1012,6 +1012,7 @@ open class BackportJSONDecoder {
         fileprivate static func _convertFromSnakeCase(_ stringKey: String) -> String {
             guard !stringKey.isEmpty else { return stringKey }
 
+#if swift(>=3.2)
             // Find the first non-underscore character
             guard let firstNonUnderscore = stringKey.index(where: { $0 != "_" }) else {
                 // Reached the end without finding an _
@@ -1052,6 +1053,48 @@ open class BackportJSONDecoder {
                 result = joinedString + String(stringKey[trailingUnderscoreRange])
             }
             return result
+#else
+            // Find the first non-underscore character
+            guard let firstNonUnderscore = stringKey.characters.index(where: { $0 != "_" }) else {
+                // Reached the end without finding an _
+                return stringKey
+            }
+
+            // Find the last non-underscore character
+            var lastNonUnderscore = stringKey.characters.index(before: stringKey.endIndex)
+            while lastNonUnderscore > firstNonUnderscore && stringKey.characters[lastNonUnderscore] == "_" {
+                stringKey.characters.formIndex(before: &lastNonUnderscore)
+            }
+
+            let keyRange = firstNonUnderscore...lastNonUnderscore
+            let leadingUnderscoreRange = stringKey.characters.startIndex..<firstNonUnderscore
+            let trailingUnderscoreRange = stringKey.characters.index(after: lastNonUnderscore)..<stringKey.characters.endIndex
+
+            var components = stringKey.characters[keyRange].split(separator: "_")
+            let joinedString: String
+            if components.count == 1 {
+                // No underscores in key, leave the word as is - maybe already camel cased
+                joinedString = String(stringKey.characters[keyRange])
+            } else {
+                joinedString = ([components[0].lowercased()] + components.suffix(from: 1).map { $0.capitalized }).joined()
+            }
+
+            // Do a cheap isEmpty check before creating and appending potentially empty strings
+            let result: String
+            if (leadingUnderscoreRange.isEmpty && trailingUnderscoreRange.isEmpty) {
+                result = joinedString
+            } else if (!leadingUnderscoreRange.isEmpty && !trailingUnderscoreRange.isEmpty) {
+                // Both leading and trailing underscores
+                result = String(stringKey.characters[leadingUnderscoreRange]) + joinedString + String(stringKey.characters[trailingUnderscoreRange])
+            } else if (!leadingUnderscoreRange.isEmpty) {
+                // Just leading
+                result = String(stringKey.characters[leadingUnderscoreRange]) + joinedString
+            } else {
+                // Just trailing
+                result = joinedString + String(stringKey.characters[trailingUnderscoreRange])
+            }
+            return result
+#endif
         }
     }
 
